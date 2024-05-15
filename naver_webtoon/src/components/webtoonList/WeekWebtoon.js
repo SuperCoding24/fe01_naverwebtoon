89310496ff4572d1c8ea85016237ac958c049052
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-05-15 18:24:16 +0900

    feat WebtoonHeader 컴포넌트 이름 오타 수정내역 확인

--- naver_webtoon/src/components/webtoonList/WeekWebtoon.js
+++ naver_webtoon/src/components/webtoonList/WeekWebtoon.js
@@ -1,14 +1,25 @@
 import React from "react";
-import WebtoonHeader from "../webtoonHeader/WebtoonHeader"
+import styled from "styled-components";
 import WebtoonCategoryHeader from "../webtoonHeader/WebtoonCategoryHeader";
 import WebtoonDayHeader from "../webtoonHeader/WebtoonDayHeader";
+import WebtoonHeader from "../webtoonHeader/WebtoonHeader";
 
 const WeekWebtoon = () => {
-  return <div>WeekWebtoon
-    <WebtooonHeader/>
-    <WebtoonCategoryHeader/>
-    <WebtoonDayHeader/>
-  </div>;
+  return (
+    <Container>
+      <WebtoonHeader/>
+      <WebtoonCategoryHeader/>
+      <WebtoonDayHeader/>
+  </Container>
+  );
 };
 
 export default WeekWebtoon;
+
+const Container = styled.div`
+  width: 1150px;
+  height: 20vh;
+  display: flex;
+  flex-direction: column;
+`;
+
